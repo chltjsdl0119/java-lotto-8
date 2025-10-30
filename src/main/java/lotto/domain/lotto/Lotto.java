3f11e860df93cb74fbd157d147702f4addba6c20
Seungwon-Choi <bestwondev@gmail.com>
2025-10-30 17:32:55 +0900

    feat: Lotto 숫자 검증 메서드 추가

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto.domain.lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,22 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        boolean isOutOfRange = numbers.stream()
+                .anyMatch(number -> number < 1 || number > 45);
+
+        if (isOutOfRange) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", 1, 45)
+            );
+        }
+    }
 }
